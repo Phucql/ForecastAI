10700b80fe29b0d3e8c9ec1fcd6c7baa27115449
unknown <phucql@uci.edu>
2025-07-03 03:40:38 -0700

    Add:Fix Forecast

--- src/components/utils/table.tsx
+++ src/components/utils/table.tsx
@@ -4,13 +4,11 @@ import * as React from "react";
 
 export const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className = "", ...props }, ref) => (
-    <div className="w-full overflow-x-auto responsive-table">
-      <table
-        ref={ref}
-        className={`w-full text-sm border-collapse min-w-[600px] md:min-w-0 ${className}`}
-        {...props}
-      />
-    </div>
+    <table
+      ref={ref}
+      className={`w-full text-sm border-collapse min-w-[600px] md:min-w-0 ${className}`}
+      {...props}
+    />
   )
 );
 Table.displayName = "Table";
