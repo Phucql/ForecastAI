dd5fac1079e0b26aad7702f6cf49551c53bd8dbe
unknown <phucql@uci.edu>
2025-07-23 20:23:08 -0700

    fix new logo and better backend

--- src/App.tsx
+++ src/App.tsx
@@ -1168,7 +1168,7 @@ function App() {
             <div className="flex items-center space-x-4">
               <img src="/logo.jpg?v=1" alt="KLUG Logo" className="h-16 w-auto" />
               <div className="flex flex-col items-center">
-                <img src="/logo1.png?v=1" alt="KLUG Logo" className="h-5 w-auto" />
+                <img src="/logo1.png?v=1" alt="KLUG Logo" className="h-6 w-auto" />
                 <div className="w-full h-0.5 bg-black my-1"></div>
                 <span className="text-orange-500 font-bold text-xl">ForecastAI</span>
               </div>
