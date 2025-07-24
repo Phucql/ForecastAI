55347167e8b0737baeab7b8a78d36044c45dfc34
unknown <phucql@uci.edu>
2025-07-23 18:25:34 -0700

    fix new logo and Klug AI

--- src/App.tsx
+++ src/App.tsx
@@ -1157,7 +1157,7 @@ function App() {
             <div className="flex items-center space-x-4">
               <img src="/logo.jpg?v=1" alt="KLUG Logo" className="h-16 w-auto" />
               <div className="flex flex-col items-center">
-                <img src="/logo1.jpg?v=1" alt="ForecastAI Logo" className="h-16 w-auto" />
+                <img src="/logo1.png?v=1" alt="ForecastAI Logo" className="h-16 w-auto" />
                 <div className="w-full h-px bg-black my-2"></div>
                 <span className="text-orange-500 font-bold text-xl">ForecastAI</span>
               </div>
